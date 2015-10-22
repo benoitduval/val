2a64e062b645f798e904e9ecb8bb5fc02445555f
Benoit Duval <benoit.duval.pro@gmail.com>
2015-10-22 18:21:20 +0200

    Make button scroll the page

--- public/js/front.js
+++ public/js/front.js
@@ -113,17 +113,17 @@ function animations() {
  * sliding 
  *  =======================================*/
 
-function sliding() {
-    $('.scrollTo, #navigation a').click(function (event) {
-    event.preventDefault();
-    var full_url = this.href;
-    var parts = full_url.split("#");
-    var trgt = parts[1];
+ function sliding() {
+     $('body').on('click', '.scrollTo, #navigation a', function (event) {
+        event.preventDefault();
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var trgt = parts[1];
 
-    $('body').scrollTo($('#' + trgt), 800, {offset: -80});
+        $('body').scrollTo($('#' + trgt), 800, {offset: -80});
 
-    });
-}
+     });
+ }
 
 /* =========================================
  * sliders 
